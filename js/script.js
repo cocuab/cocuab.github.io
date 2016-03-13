17ff9e637fb1cbd4f28878383c6e939abfec65a6
cocuab <ajonga@uab.edu>
2016-03-13 17:42:50 -0500

    revamp

---
+++ js/script.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+  $('.single-item').slick({
+    dots: true,
+	slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 3000
+  });
+});
+
+$('.center').slick({
+  centerMode: true,
+  centerPadding: '60px',
+  slidesToShow: 3,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
+});
