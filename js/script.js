8f7568a09929b20f83bbb59aaf10b1cbef1aa403
cocuab <ajonga@uab.edu>
2016-03-13 19:35:49 -0500

    pub

--- js/script.js
+++ js/script.js
@@ -3,7 +3,12 @@ $(document).ready(function() {
     dots: true,
 	slidesToScroll: 1,
   autoplay: true,
-  autoplaySpeed: 3000
+  autoplaySpeed: 2500,
+  dots: true,
+  infinite: true,
+  speed: 500,
+  fade: true,
+  cssEase: 'linear'
   });
 });
 
